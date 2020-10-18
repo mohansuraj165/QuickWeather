d42cccad028460d65e8d6581fe3c786673d7d079
Mohan Suraj Lakshmi Narayana <37024382+mohansuraj165@users.noreply.github.com>
2020-10-18 01:05:56 -0400

    Adding image resources

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -3,18 +3,47 @@ const images = require.context("../../Images", true);
 
 const DisplayForecast = (props) => {
   window.weatherData = props.weatherData;
-  let img = images("./" + props.weatherData.weather[0].main + ".jpg");
+
+  let img;
+
+  try {
+       img = images("./" + props.weatherData.weather[0].main + ".jpg")
+  } catch(err) {
+      if (props.weatherData.weather[0].main.toLowerCase().includes("cloud")) {
+          img = "./Clouds.jpg"
+      }
+      else if (props.weatherData.weather[0].main.toLowerCase().includes("sun") ||
+          props.weatherData.weather[0].main.toLowerCase().includes("clear")) {
+          img = "./Clear.jpg"
+      }
+
+      else if (props.weatherData.weather[0].main.toLowerCase().includes("snow")) {
+          img = "./Snow.jpg"
+      }
+
+      else if (props.weatherData.weather[0].main.toLowerCase().includes("rain") ||
+          props.weatherData.weather[0].main.toLowerCase().includes("shower")) {
+          img = "./Rain.jpg"
+      }
+      else if (props.weatherData.weather[0].main.toLowerCase().includes("storm")) {
+          img = "./Thunder.jpg"
+      }
+      else {
+          img = images("./Default.jpg");
+      }
+  }
   let sunrise = new Date(props.weatherData.sys.sunrise);
   return (
-    <div>
+    <div class="align-items-center" >
       {props.weatherData.cod === 200 ? (
-        <div>
-          <div className="row">
-            <div className="col s12 m7">
-              <div className="card">
-                <div className="card-image">
+          //<div className="container">
+        <div class="text-center" style={{width: 750, height: 750}}>
+          <div className="row"  >
+            <div className="col s12 m7" >
+              <div className="card" >
+                <div className="card-image"  >
                   <img src={img} alt="weather" />
-                  <span className="card-title">
+                  <span className="card-title" >
                     {props.weatherData.name} -{" "}
                     {props.weatherData.weather[0].description}
                   </span>
@@ -30,6 +59,7 @@ const DisplayForecast = (props) => {
             </div>
           </div>
         </div>
+          //</div>
       ) : null}
     </div>
   );
